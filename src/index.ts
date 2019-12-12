96e73402e241702c9c90d390ae654546d3927309
D <danixoon@gmail.com>
2019-12-12 11:44:04 +0300

    removed test command

--- src/index.ts
+++ src/index.ts
@@ -10,68 +10,51 @@ dotenv.config();
 
 const { api } = new VK({ token: process.env.TOKEN });
 
-yargs
-  .command(
-    "test",
-    "Test command",
-    y => {
-      y.option("foo", {})
-        .option("who", {})
-        .check(({ foo, who }) => {
-          if (foo === undefined && who === undefined) throw new Error("foo or who option required");
-          return true;
-        })
-        .conflicts("who", "foo");
-    },
-    argv => {
-      console.log(argv);
-    }
-  )
+yargs.command(
+  `message [${PEER_ID}]`,
+  "Seinging message to the dialog",
+  y => {
+    y.positional(PEER_ID, { describe: "peer id for message", default: DEFAULT_PEER_ID })
+      .option("content", {
+        description: "message text",
+        alias: "c"
+      })
+      .option("attachment", {
+        description: "message attachments",
+        alias: "a",
+        type: "array"
+      })
+      .check(({ content, attachment }) => {
+        if (content === undefined && attachment === undefined) throw new Error("content or attachment option required");
+        return true;
+      });
+  },
+  async argv => {
+    const { content, attachment, peerId } = argv as typeof argv & { peerId: number; content?: string; attachment?: string[] };
+    const parseAttachments = async () => {
+      if (!attachment) return "";
+      const parsed = await Promise.all(
+        attachment.map(async (a, i) => {
+          const regexpId = /-\d+|\d+/g;
 
-  .command(
-    `message [${PEER_ID}]`,
-    "Seinging message to the dialog",
-    y => {
-      y.positional(PEER_ID, { describe: "peer id for message", default: DEFAULT_PEER_ID })
-        .option("content", {
-          description: "message text",
-          alias: "c"
-        })
-        .option("attachment", {
-          description: "message attachments",
-          alias: "a",
-          type: "array"
-        })
-        .check(({ content, attachment }) => {
-          if (content === undefined && attachment === undefined) throw new Error("content or attachment option required");
-          return true;
-        });
-    },
-    async argv => {
-      const { content, attachment, peerId } = argv as typeof argv & { peerId: number; content?: string; attachment?: string[] };
-      const parseAttachments = async () => {
-        if (!attachment) return "";
-        const parsed = await Promise.all(
-          attachment.map(async (a, i) => {
-            const [type] = /\w+/.exec(a);
-            const regexp = /-\d+|\d+/g;
-            const [ownerId] = regexp.exec(a),
-              [id] = regexp.exec(a);
+          const [type] = /\w+/.exec(a);
+          const [ownerId] = regexpId.exec(a),
+            [id] = regexpId.exec(a);
 
-            switch (type) {
-              case "album": {
-                const photos = await api.photos.get({ album_id: id, owner_id: Number(ownerId), count: 200 });
-                const photo = photos.items[_.random(photos.items.length)];
-                a = `photo${photo.owner_id}_${photo.id}`;
-                break;
-              }
+          switch (type) {
+            case "album": {
+              const photos = await api.photos.get({ album_id: id, owner_id: Number(ownerId), count: 200 });
+              const photo = photos.items[_.random(photos.items.length)];
+              a = `photo${photo.owner_id}_${photo.id}`;
+              break;
             }
-            return a;
-          })
-        );
+          }
+          return a;
+        })
+      );
 
-        return parsed.join();
-      };
-      api.messages.send({ peer_id: peerId, message: content || "", attachment: await parseAttachments() });
-    }
-  ).argv;
+      return parsed.join();
+    };
+    api.messages.send({ peer_id: peerId, message: content || "", attachment: await parseAttachments() });
+  }
+).argv;
